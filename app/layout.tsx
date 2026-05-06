5d13577e09c08556bdfc8906b4d579670a1c2f33
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-05-06 12:34:29 +0530

    basic structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />        
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,400;1,6..72,400;0,6..72,600;1,6..72,600&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet"/>
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
