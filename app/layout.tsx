3d8f1a16f841427c22829d1eb4eeaef39dee0133
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-05-07 21:10:49 +0530

    feat: Update fonts and styling for UI elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />        
-        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,400;1,6..72,400;0,6..72,600;1,6..72,600&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,100;0,6..72,200;0,6..72,400;0,6..72,600;1,6..72,100;1,6..72,200;1,6..72,400;1,6..72,600&display=swap" rel="stylesheet"/>        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@1,400..700&display=swap" rel="stylesheet"/>
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export default function Page() {
     <>
       <nav className="flex flex-row items-center justify-between px-10 py-5"> 
         <div className="flex items-center gap-8">
-          <span style={{ fontFamily:"'Newsreader', serif", fontWeight: 700 }} className="font-bold text-[#0E1828] tracking-widest text-[24px]" >
+          <span style={{ fontFamily:"'Newsreader', serif"}} className="font-bold text-[#0E1828] tracking-widest text-[24px]" >
             PRIORA
           </span>
-          <span className="text-[9px] tracking-widest border-2 border-[#C45C2A] text-[#C45C2A] px-2 py-0.5 specialFont">
+          <span className="text-[9px] tracking-widest border-1 border-[#C45C2A] text-[#C45C2A] px-1.5 py-0.5 specialFont">
             BETA
           </span>
         </div>
@@ -21,8 +21,22 @@ export default function Page() {
       </nav>  
       <main className="">
         <section className=" grid grid-cols-2 h-screen">
-          <div className="flex flex-col justify-center gap-6 md:gap-8 px-8 md:px-12 lg:px-16 bg-[#F5F0E8]">
-              <h1 style={{ fontFamily:"'Newsreader', serif" }}className="text-4xl font-light md:text-5xl lg:text-[70px] leading-[64.8px]">Messy<br/> feedback. <br/><span className="italic">Clear<br/>decisions.</span></h1>
+          <div className="flex flex-col justify-start pt-[65px] md:pt-[125px] lg:pt-[190px] bg-[#F5F0E8] gap-6 md:gap-8 px-8 md:px-12 lg:px-24 ">
+            <h2 className="specialFont tracking-[4px] leading-[15px] text-[#C8430A] text-[10px] mb-5">SYSTEMIC SYNTHESIS ENGINE</h2>
+            <h1 style={{ fontFamily: "'Newsreader', serif" }} className="text-4xl sm:text-[55px] md:text-[65px] lg:text-[75px] leading-[64.8px]">
+              <span className="">Messy feedback.</span>
+              <br/>
+              <span style={{ fontWeight: 200 }} className="italic">Clear decisions.</span>
+            </h1>
+            <p className="text-[16.5px] text-[#0F1F3D]/70 paraFont mt-7 leading-[26px]">
+              Transforming qualitative noise into quantitative strategic <br/>alignment through architectural data synthesis
+            </p>
+            <button className="specialFont flex flex-row gap-6 uppercase tracking-[3.3px] leading-[16.5px] px-13 py-[22px] bg-[#0F1F3D] w-fit text-white text-[11px] mt-5">
+              Begin Analysis
+              <svg xmlns="http://www.w3.org/2000/svg" className="mt-[3px]" width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+              </svg>
+            </button>
           </div>
           <div className="bg-[#0F1F3D]">
 
